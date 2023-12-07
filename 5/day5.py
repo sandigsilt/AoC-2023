4492f1292fd8db3525b8746c897ec4f2a1d9b479
Erik Johansson <erikjoh96@gmail.com>
2023-12-07 07:37:36 +0100

    test

--- 5/day5.py
+++ 5/day5.py
@@ -1,29 +1,35 @@
-import numpy as np
+# import numpy as np
 
-f = open(r"C:\Users\erijoh\OneDrive - ELU\Documents\Python\AoC-2023\5\test5.txt", "r")
+# def shift_right(lst, n):
+#     n = n % len(lst)  # Ensure n is within the range of the list length
+#     shifted_list = lst[-n:] + lst[:-n]
+#     return shifted_list
 
+# def shift_left(lst, n):
+#     n = n % len(lst)  # Ensure n is within the range of the list length
+#     shifted_list = lst[n:]# + lst[:n]
+#     return shifted_list
 
-seed_to_soil_map = []
-
-strings = f.readlines()
-
-for i in range(len(strings)):
-    string = strings[i]
-    if string.startswith("seed-to-soil"):
-        jj = i+1
-        while strings[jj] != "\n":
-            seed_to_soil_map.append(strings[jj].replace("\n",""))
-            jj = jj+1
+# f = open(r"C:\Users\erijoh\OneDrive - ELU\Documents\Python\AoC-2023\5\test5.txt", "r")
 
 
-print(seed_to_soil_map)
-seedvec = np.zeros([100,2])
+# seed_to_soil_map = []
 
-for seedmap in seed_to_soil_map:
-    seedmap = seedmap.split(" ")
-    seedrange = seedmap[2]
-    for i in range(int(seedrange)):
-        seedvec[int(seedmap[0])+int(seedrange)] = 1 #int(seedvec[seedmap[0]+seedrange]) + seedrange
-        pass
+# strings = f.readlines()
 
-print(seedmap)
+# for i in range(len(strings)):
+#     string = strings[i]
+#     if string.startswith("seed-to-soil"):
+#         jj = i+1
+#         while strings[jj] != "\n":
+#             seed_to_soil_map.append(strings[jj].replace("\n",""))
+#             jj = jj+1
+
+
+# print(seed_to_soil_map)
+# seedvec = np.zeros([100,2])
+
+# linvec = [*range(0,99)]
+
+# linvec = shift_left(linvec,2)
+
